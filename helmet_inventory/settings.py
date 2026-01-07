a9c6ba1146138a54e19cb1385ff197fba8cf7c74
M-IYAAADH <mohamediyaadh144@gmail.com>
2026-01-07 13:42:23 +0500

    Switch database from SQLite to PostgreSQL

--- helmet_inventory/settings.py
+++ helmet_inventory/settings.py
@@ -5,6 +5,7 @@ Django settings for helmet_inventory project.
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Load environment variables from .env (local) or Render env vars
 load_dotenv()
@@ -103,10 +104,11 @@ WSGI_APPLICATION = "helmet_inventory.wsgi.application"
 # =========================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
